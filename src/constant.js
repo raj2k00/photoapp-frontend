59410bd0ce308014075c5533485a1f9b585995b1
Mohanraj G <mohan@leaguex.com>
2022-11-11 17:51:01 +0530

    fix: app url change

--- src/constant.js
+++ src/constant.js
@@ -1,2 +1,2 @@
-export const APP_URL = "https://pacific-lake-87005.herokuapp.com/";
+export const APP_URL = "https://pacific-lake-87005.herokuapp.com/api/v1/";
 // export const APP_URL = "http://127.0.0.1:4000/api/v1/";
